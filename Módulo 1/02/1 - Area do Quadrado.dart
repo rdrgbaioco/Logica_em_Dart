4a8f80d4781613f169f57262c9bcde961723a176
Samuel Rodrigo Baioco <rdrgbaioco@gmail.com>
2022-05-24 10:04:19 -0400

    update

--- Módulo 1/02/1 - Area do Quadrado.dart
+++ Módulo 1/02/1 - Area do Quadrado.dart
@@ -1,8 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
 
 void main() {
   stdout.write("Informe o tamanho: ");
-  var tamanhoInt = int.parse(stdin.readLineSync().toString());
-  final resultado = tamanhoInt * tamanhoInt;
+  var tamanho = double.parse(stdin.readLineSync(encoding: utf8).toString());
+  final resultado = tamanho * tamanho;
   print("O resultado é: $resultado");
 }
